fdf2b8cd9507e06862a8f84decf8b42692486ddf
Maxim Tolstokory <maxim.code@outlook.com>
2022-02-15 19:11:12 -0900

    feat: set up App starter code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,21 @@
+import { useState, useContext } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { CurrentUserContext } from '../../contexts/CurrentUserContext';
+
 function App() {
+
+  //context state variables
+  const [currentUser, setCurrentUser] = useState({ name: '', email: '' });
+  const {isLoggedIn} = true;
+
   return (
-    <></>
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <Routes>
+          <Route path="*" Element={<Navigate replace to="/" />}/>
+        </Routes>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
