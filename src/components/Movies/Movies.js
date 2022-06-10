7a9dbcd7d8330c9789cda09406c0b26fdf987fb0
Maxim Tolstokory <maxim.code@outlook.com>
2022-06-10 13:35:27 -0800

    feat: add Search component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,12 +1,13 @@
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Search from '../Search/Search';
 
 export default function Movies(props) {
   return (
     <>
       <Header isLoggedIn={props.isLoggedIn}  />
       <main className="Movies">
-
+        <Search />
       </main>
       <Footer />
     </>
---
+++ src/components/Search/Search.js
@@ -0,0 +1,19 @@
+export default function Search() {
+  return (
+    <section className="Search">
+      <form action="" className="Search__form">
+        <div className="Search__container">
+          <input type="text" placeholder="Фильм" className="Search__input"/>
+          <button className="Search__button" type="button"></button>
+        </div>
+        <div className="Search__label">
+          <label htmlFor="switch" className="Search__slider-box">
+            <input id="switch" type="checkbox" className="Search__checkbox"/>
+            <span className="Search__slider"></span>
+          </label>
+          <span className="Search__label-text">Короткометражки</span>
+        </div>
+      </form>
+    </section>
+  );
+}
