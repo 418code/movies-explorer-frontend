b5d7baf40d14d713e83f8d73929c5023cefebfe8
Maxim Tolstokory <maxim.code@outlook.com>
2022-06-22 16:19:45 -0800

    fix: enable go back button

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,14 +1,17 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function NotFound() {
+
+  const navigate = useNavigate();
+
   return (
       <>
-        <section className="NotFound">
+        <main className="NotFound">
           <p className="NotFound__header">404</p>
           <p className="NotFound__message">Страница не найдена</p>
-        </section>
+        </main>
         <aside>
-          <Link to={"#"} className="App__link App__link_size_adaptive App__link_color_orange App__link_position_bottom">Назад</Link>
+          <button onClick={() => navigate(-1)} className="NotFound__button App__link App__link_size_adaptive App__link_color_orange App__link_position_bottom">Назад</button>
         </aside>
       </>
   );
