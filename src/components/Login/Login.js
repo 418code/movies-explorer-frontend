65e90bd081c8b412a8ec273652139471bd342f0a
Maxim Tolstokory <maxim.code@outlook.com>
2022-06-22 19:46:37 -0800

    fix: add form validation

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -6,9 +6,9 @@ export default function Login() {
     <EnterForm name="loginForm" formHeader="Рады видеть!">
       <div className="EnterForm__fields">
         <label className="EnterForm__label">E-mail</label>
-        <input type="email" className="EnterForm__input EnterForm__input_type_error" text="pochta@yandex.ru" placeholder="pochta@yandex.ru" />
+        <input type="email" className="EnterForm__input EnterForm__input_type_error" text="pochta@yandex.ru" placeholder="pochta@yandex.ru" minlength="3" maxlength="128"/>
         <label className="EnterForm__label">Пароль</label>
-        <input type="password" className="EnterForm__input" placeholder="••••••••••••••"/>
+        <input type="password" className="EnterForm__input" placeholder="••••••••••••••" minlength="3" maxlength="128"/>
         <span className="EnterForm__input-error EnterForm__input-error_active">Что-то пошло не так...</span>
       </div>
       <button className="EnterForm__button">Войти</button>
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -6,11 +6,11 @@ export default function Register() {
     <EnterForm name="registerForm" formHeader="Добро пожаловать!">
       <div className="EnterForm__fields">
         <label className="EnterForm__label">Имя</label>
-        <input type="text" className="EnterForm__input" placeholder="Виталий" />
+        <input type="text" className="EnterForm__input" placeholder="Виталий" minlength="2" maxlength="64" />
         <label className="EnterForm__label">E-mail</label>
-        <input type="email" className="EnterForm__input EnterForm__input_type_error" text="pochta@yandex.ru" placeholder="pochta@yandex.ru" />
+        <input type="email" className="EnterForm__input EnterForm__input_type_error" text="pochta@yandex.ru" placeholder="pochta@yandex.ru" minlength="3" maxlength="128"/>
         <label className="EnterForm__label">Пароль</label>
-        <input type="password" className="EnterForm__input" placeholder="••••••••••••••"/>
+        <input type="password" className="EnterForm__input" placeholder="••••••••••••••" minlength="8" maxlength="128"/>
         <span className="EnterForm__input-error EnterForm__input-error_active">Что-то пошло не так...</span>
       </div>
       <button className="EnterForm__button">Зарегистрироваться</button>
