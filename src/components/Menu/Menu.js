0840826ccefa76772b96bd8a502568f866656416
Maxim Tolstokory <maxim.code@outlook.com>
2022-06-22 16:44:02 -0800

    fix: adjust Menu links

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -13,10 +13,10 @@ export default function Menu() {
             <Link to="/movies" className="App__link App__link_style_undrl-bld App__link_position_menu App__link_size_large">Фильмы</Link>
           </li>
           <li className="Menu__list-item" key="saved-movies">
-            <Link to="/saved" className="App__link App__link_position_menu App__link_size_large">Сохранённые фильмы</Link>
+            <Link to="/saved-movies" className="App__link App__link_position_menu App__link_size_large">Сохранённые фильмы</Link>
           </li>
         </ul>
-        <Link to="/account" className="App__link App__link_position_menu App__link_style_btn-rnd App__link_size_med">Аккаунт</Link>
+        <Link to="/profile" className="App__link App__link_position_menu App__link_style_btn-rnd App__link_size_med">Аккаунт</Link>
       </nav>
     </div>
   );
