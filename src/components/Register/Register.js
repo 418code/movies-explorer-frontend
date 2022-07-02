05f79e39e1b67ab2fb8025ec8421067bb5705dc7
Maxim Tolstokory <maxim.code@outlook.com>
2022-07-01 20:27:26 -0800

    feat: enable registration validation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,34 +1,14 @@
 import EnterForm from '../EnterForm/EnterForm';
+import { useFormWithValidation } from '../FormValidate/FormValidate';
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 export default function Register(props) {
 
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const handleNameChange = (e) => {
-    setName(e.target.value);
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const resetForm = () => {
-    setName('');
-    setEmail('');
-    setPassword('');
-  };
+  const { values, handleChange, errors, isValid, resetForm } = useFormWithValidation();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.onRegister(name, email, password);
+    props.onRegister(values['name'], values['email'], values['password']);
     resetForm();
   }
 
@@ -36,14 +16,21 @@ export default function Register(props) {
     <EnterForm name="registerForm" formHeader="Добро пожаловать!" onSubmit={handleSubmit}>
       <div className="EnterForm__fields">
         <label className="EnterForm__label">Имя</label>
-        <input type="text" name="registerName" className="EnterForm__input" placeholder="Виталий" minLength="2" maxLength="64" onChange={handleNameChange} value={name} required/>
+        <input name="name" type="text" className={`EnterForm__input ${errors['name'] ? 'EnterForm__input_type_error' : ''}`}
+         placeholder="Виталий" minLength="3" maxLength="64"
+         value={values['name'] || ''} onChange={handleChange} required/>
+        <span className={`EnterForm__input-error ${!isValid ? 'EnterForm__input-error_active' : ''}`}>{errors['name']}</span>
         <label className="EnterForm__label">E-mail</label>
-        <input type="email" className="EnterForm__input EnterForm__input_type_error" text="pochta@yandex.ru" placeholder="pochta@yandex.ru" minLength="3" maxLength="128" onChange={handleEmailChange} value={email} required/>
+        <input name="email" type="email" className={`EnterForm__input ${errors['email'] ? 'EnterForm__input_type_error' : ''}`}
+         placeholder="pochta@yandex.ru" minLength="3" maxLength="128"
+         value={values['email'] || ''} onChange={handleChange} required/>
+        <span className={`EnterForm__input-error ${!isValid ? 'EnterForm__input-error_active' : ''}`}>{errors['email']}</span>
         <label className="EnterForm__label">Пароль</label>
-        <input type="password" className="EnterForm__input" placeholder="••••••••••••••" minLength="8" maxLength="128" onChange={handlePasswordChange} value={password} required/>
-        <span className="EnterForm__input-error EnterForm__input-error_active">Что-то пошло не так...</span>
+        <input name="password" type="password" className={`EnterForm__input ${errors['password'] ? 'EnterForm__input_type_error' : ''}`}
+         placeholder="••••••••••••••" minLength="8" maxLength="64" value={values['password'] || ''} onChange={handleChange} required/>
+        <span className={`EnterForm__input-error ${!isValid ? 'EnterForm__input-error_active' : ''}`}>{errors['password']}</span>
       </div>
-      <button className="EnterForm__button">Зарегистрироваться</button>
+      <button className={`EnterForm__button ${!isValid ? 'EnterForm__button_disabled' : ''}`} disabled={!isValid}>Зарегистрироваться</button>
       <p className="EnterForm__text">
         Уже зарегистрированы? <Link to="/signin" className="App__link App__link_color_orange">Войти</Link>
       </p>
