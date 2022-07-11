da603febe86e92dc48fcd3e47965c16d6094e78c
Maxim Tolstokory <maxim.code@outlook.com>
2022-07-11 11:21:19 -0800

    chore: upgrade react to v18

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App/App';
@@ -7,7 +7,8 @@ import reportWebVitals from './reportWebVitals';
 import AuthWrapper from './components/AuthWrapper/AuthWrapper';
 import { CookiesProvider } from 'react-cookie';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthWrapper>
@@ -17,7 +18,6 @@ ReactDOM.render(
       </AuthWrapper>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
